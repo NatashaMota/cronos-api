cccc8bb67b6ace5d8ce2cf88a490c756b21d883a
MotaNT <natasha.silva4@fatec.sp.gov.br>
2023-02-27 13:45:22 -0300

    initial commit

---
+++ src/main/java/br/com/cronosapi/CronosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cronosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CronosApiApplication.class, args);
+    }
+
+}
